3f763518bef6ce4122e498bb1ea2f52619d22e5f
mattdeitke <mdeitke23@gmail.com>
2023-02-01 19:52:28 +0000

    add link to gh comment

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ tar -xf blender-3.2.2-linux-x64.tar.xz
 rm blender-3.2.2-linux-x64.tar.xz
 
 # this is needed to download urls in blender
+# https://github.com/python-poetry/poetry/issues/5117#issuecomment-1058747106
 sudo update-ca-certificates --fresh
 export SSL_CERT_DIR=/etc/ssl/certs
 
