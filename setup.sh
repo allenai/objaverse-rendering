521885d183243718f71ab581b369899928ebba46
mattdeitke <mdeitke23@gmail.com>
2023-02-01 19:51:51 +0000

    add update ssl cert to setup commands

--- setup.sh
+++ setup.sh
@@ -2,5 +2,9 @@ wget https://download.blender.org/release/Blender3.2/blender-3.2.2-linux-x64.tar
 tar -xf blender-3.2.2-linux-x64.tar.xz
 rm blender-3.2.2-linux-x64.tar.xz
 
+# this is needed to download urls in blender
+sudo update-ca-certificates --fresh
+export SSL_CERT_DIR=/etc/ssl/certs
+
 sudo python3 ai2thor-xorg.py start || true
 export DISPLAY=:0.0
