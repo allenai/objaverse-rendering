085e97aa52e3d9e953680e4790cd65a8e3501ef7
mattdeitke <mdeitke23@gmail.com>
2023-02-01 06:18:07 +0000

    correct setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-RUN wget https://download.blender.org/release/Blender3.2/blender-3.2.2-linux-x64.tar.xz
-RUN tar -xf blender-3.2.2-linux-x64.tar.xz
-RUN rm blender-3.2.2-linux-x64.tar.xz
+wget https://download.blender.org/release/Blender3.2/blender-3.2.2-linux-x64.tar.xz
+tar -xf blender-3.2.2-linux-x64.tar.xz
+rm blender-3.2.2-linux-x64.tar.xz
 
-sudo python3 ai2thor-xorg.py start
+sudo python3 ai2thor-xorg.py start || true
 export DISPLAY=:0.0
