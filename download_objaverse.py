fef9159a48dace22db9acef876f8465ac3ccb078
mattdeitke <mdeitke23@gmail.com>
2023-02-05 04:25:21 +0000

    add support for get_completed_uids

--- download_objaverse.py
+++ download_objaverse.py
@@ -1,7 +1,26 @@
 import json
 import random
 
+import boto3
 import objaverse
+from tqdm import tqdm
+
+
+def get_completed_uids():
+    # get all the files in the objaverse-images bucket
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket("objaverse-images")
+    bucket_files = [obj.key for obj in tqdm(bucket.objects.all())]
+
+    bucket_files[:10]
+    dir_counts = {}
+    for file in bucket_files:
+        d = file.split("/")[0]
+        dir_counts[d] = dir_counts.get(d, 0) + 1
+
+    # get the directories with 12 files
+    dirs = [d for d, c in dir_counts.items() if c == 12]
+    return set(dirs)
 
 # set the random seed to 42
 random.seed(42)
@@ -11,7 +30,12 @@ uids = objaverse.load_uids()
 random.shuffle(uids)
 
 object_paths = objaverse._load_object_paths()
-uids = uids[:1000]
+uids = uids[:150_000]
+
+# get the uids that have already been downloaded
+completed_uids = get_completed_uids()
+uids = [uid for uid in uids if uid not in completed_uids]
+
 uid_object_paths = [f"https://huggingface.co/datasets/allenai/objaverse/resolve/main/{object_paths[uid]}" for uid in uids]
 
 with open("input_model_paths.json", "w") as f:
