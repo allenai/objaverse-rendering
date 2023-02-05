036b1b4bfae008c8cb462e50c4fe85814781ed93
mattdeitke <28768645+mattdeitke@users.noreply.github.com>
2023-02-05 14:56:34 -0800

    add start_i and end_i to parser

--- download_objaverse.py
+++ download_objaverse.py
@@ -1,9 +1,19 @@
+import argparse
 import json
 import random
 
 import boto3
 import objaverse
 from tqdm import tqdm
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--start_i", type=int, required=True, help="total number of files uploaded"
+)
+parser.add_argument(
+    "--end_i", type=int, required=True, help="total number of files uploaded"
+)
+args = parser.parse_args()
 
 
 def get_completed_uids():
@@ -22,6 +32,7 @@ def get_completed_uids():
     dirs = [d for d, c in dir_counts.items() if c == 12]
     return set(dirs)
 
+
 # set the random seed to 42
 random.seed(42)
 
@@ -30,13 +41,16 @@ uids = objaverse.load_uids()
 random.shuffle(uids)
 
 object_paths = objaverse._load_object_paths()
-uids = uids[:150_000]
+uids = uids[args.start_i : args.end_i]
 
 # get the uids that have already been downloaded
 completed_uids = get_completed_uids()
 uids = [uid for uid in uids if uid not in completed_uids]
 
-uid_object_paths = [f"https://huggingface.co/datasets/allenai/objaverse/resolve/main/{object_paths[uid]}" for uid in uids]
+uid_object_paths = [
+    f"https://huggingface.co/datasets/allenai/objaverse/resolve/main/{object_paths[uid]}"
+    for uid in uids
+]
 
 with open("input_model_paths.json", "w") as f:
     json.dump(uid_object_paths, f, indent=2)
