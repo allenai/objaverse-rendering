a5ab0a7bbdd89bc8b92038b9028d11a1aebe69e2
Matt Deitke <mdeitke23@gmail.com>
2023-02-09 09:11:48 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+# install blender 3.2.2
 wget https://download.blender.org/release/Blender3.2/blender-3.2.2-linux-x64.tar.xz
 tar -xf blender-3.2.2-linux-x64.tar.xz
 rm blender-3.2.2-linux-x64.tar.xz
@@ -8,3 +9,4 @@ sudo update-ca-certificates --fresh
 export SSL_CERT_DIR=/etc/ssl/certs
 
 sudo python3 ai2thor-xorg.py start || true
+pip install -r requirements.txt
